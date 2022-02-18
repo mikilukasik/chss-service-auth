f80003fa038b9b198cad14798fefce2664fa7aca
mikilukasik <szarhazi@gmail.com>
2022-02-18 16:24:53 +0000

    less logging

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -5,22 +5,26 @@ let db;
 
 const collections = {};
 
-export const connect = async() => {
+export const connect = async () => {
   await client.connect();
-  console.log('Connected successfully to server');
+  // console.log('Connected successfully to server');
   db = client.db('chss');
 };
 
-export const getCollection = async(collecitonName) => {
+export const getCollection = async (collecitonName) => {
   if (collections[collecitonName]) return collections[collecitonName];
   if (!db) await connect();
   collections[collecitonName] = db.collection(collecitonName);
   return collections[collecitonName];
 };
 
-export const getNextAvailableUserId = async() => {
+export const getNextAvailableUserId = async () => {
   const countersCollection = await getCollection('counters');
-  const response = await countersCollection.findOneAndUpdate({ _id: 'nextUserId' }, { $inc: { userId: 1 } }, { upsert: true });
+  const response = await countersCollection.findOneAndUpdate(
+    { _id: 'nextUserId' },
+    { $inc: { userId: 1 } },
+    { upsert: true },
+  );
   if (response.lastErrorObject.upserted === 'nextUserId') return 0;
   return response.value.userId;
-}
+};
